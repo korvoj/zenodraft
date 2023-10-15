d74e057ab806c7e2d3c0136e7958576a15baf8e5
korvoj <korvoj@users.noreply.github.com>
2023-10-15 09:49:49 +0000

    Fix 415 - UNSUPPORTED MEDIA TYPE error as a result of the new API

--- src/lib/file/add.ts
+++ src/lib/file/add.ts
@@ -11,17 +11,10 @@ export const file_add = async (token: string, sandbox: boolean, id: string, file
     }
     const deposition = await deposition_show_details(token, sandbox, id, 'version', verbose)
     const bucket = deposition.links.bucket
-    let content_type: string = mime.contentType(filename) ? mime.contentType(filename) as string : 'text/plain'
-    if (content_type.includes('application/json')) {
-        // zenodo declines json uploads with a 400 - BAD REQUEST,  
-        // avoiding error by setting content type to plain text
-        content_type = 'text/plain'
-    }
     const stream = fs.createReadStream(filename);
     const method = 'PUT'
     const headers = {
         'Authorization': `Bearer ${token}`,
-        'Content-Type': content_type,
         'Content-Length': (fs.statSync(filename).size).toString()
     }
     const init: RequestInit = { method, headers, body: stream }
